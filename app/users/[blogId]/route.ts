d9112cb8fbc557324aa5f9f83da557b0f3b3d9a6
Jihyeok Seo <limeburst@icloud.com>
2023-08-05 14:55:57 +0900

    WIP

---
+++ app/users/[blogId]/route.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { blogId: string }}) {
+    const url = process.env.NEXT_PUBLIC_URL;
+    const blogId = params.blogId;
+
+    const blog = await prisma.blog.findUnique({
+        where: {
+            slug: blogId
+        }
+    });
+
+    if (!blog) {
+        return NextResponse.redirect(`/`);
+    }
+
+    return NextResponse.json({
+        "@context": [
+            "https://www.w3.org/ns/activitystreams",
+            "https://w3id.org/security/v1"
+        ],
+        "id": `${url}/users/${blogId}`,
+        "type": "Person",
+        "following": `${url}/users/${blogId}/following`,
+        "followers": `${url}/users/${blogId}/followers`,
+        "inbox": `${url}/inbox`,
+        "preferredUsername": blogId,
+        "name": blogId,
+        "summary": blog.description,
+        "url": `${url}/users/${blogId}`,
+        "manuallyApprovesFollowers": false,
+        "discoverable": true,
+        "published": blog.createdAt,
+    })
+}
--- app/users/[handle]/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(request: NextRequest, { params }: { params: { username: string }}) {
-    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${params.username}`);
-}
