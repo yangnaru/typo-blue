fa748439d5b8f7891d0c3fcb3c774e9bede7c9b9
Jihyeok Seo <ji@hyeok.org>
2024-07-11 23:48:48 +0900

    admin: Link to latest blog post

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/table";
 import { prisma } from "@/lib/db";
 import ImpersonateButton from "./impersonate-button";
+import Link from "next/link";
+import { encodePostId } from "@/lib/utils";
 
 export default async function AdminRootPage() {
   await assertAdmin();
@@ -41,17 +43,27 @@ export default async function AdminRootPage() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {blogs.map((blog) => (
-            <TableRow key={blog.id}>
-              <TableCell>{blog.slug}</TableCell>
-              <TableCell>
-                {blog.posts[0] ? blog.posts[0].title : "글 없음"}
-              </TableCell>
-              <TableCell>
-                <ImpersonateButton userId={blog.userId} />
-              </TableCell>
-            </TableRow>
-          ))}
+          {blogs.map((blog) => {
+            const encodedLatestPostId = encodePostId(blog.posts[0]?.uuid);
+
+            return (
+              <TableRow key={blog.id}>
+                <TableCell>{blog.slug}</TableCell>
+                <TableCell>
+                  {blog.posts[0] ? (
+                    <Link href={`/@${blog.slug}/${encodedLatestPostId}`}>
+                      {blog.posts[0].title}
+                    </Link>
+                  ) : (
+                    "글 없음"
+                  )}
+                </TableCell>
+                <TableCell>
+                  <ImpersonateButton userId={blog.userId} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </div>
