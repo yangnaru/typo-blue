573c731532391b14b363602d609d7b36abb6c4fe
Jihyeok Seo <ji@hyeok.org>
2024-07-11 23:15:22 +0900

    shadcn/ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,8 @@ import {
   encode as base62encode,
   decode as base62decode,
 } from "@urlpack/base62";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function encodePostId(uuid: string) {
   return base62encode(Buffer.from(uuid.replaceAll("-", ""), "hex"));
@@ -9,4 +11,8 @@ export function encodePostId(uuid: string) {
 
 export function decodePostId(id: string) {
   return Buffer.from(base62decode(id)).toString("hex");
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
