b4a6765e32d3d8d10df6ba2e636f4a657fcba6e9
Jihyeok Seo <ji@hyeok.org>
2025-08-01 18:31:06 +0900

    Fix HTML semantics

--- components/DisableFederationButton.tsx
+++ components/DisableFederationButton.tsx
@@ -61,16 +61,14 @@ export function DisableFederationButton({
             이 작업은 되돌릴 수 없습니다. 다음과 같은 작업이 수행됩니다:
             <br />
             <br />
-            <div className="space-y-1">
-              • 연합우주에 게시물 삭제 요청을 보냅니다.
-              <br />
-              • 연합우주의 특성 상, 모든 게시물이 삭제되지 않을 수 있습니다.
-              <br />
-              • 모든 팔로워와의 연결이 해제됩니다.
-              <br />
-              • ActivityPub 프로필이 완전히 제거됩니다.
-              <br /> <br />
-            </div>
+            • 연합우주에 게시물 삭제 요청을 보냅니다.
+            <br />
+            • 연합우주의 특성 상, 모든 게시물이 삭제되지 않을 수 있습니다.
+            <br />
+            • 모든 팔로워와의 연결이 해제됩니다.
+            <br />
+            • ActivityPub 프로필이 완전히 제거됩니다.
+            <br /> <br />
             정말로 연합우주를 비활성화하시겠습니까?
           </AlertDialogDescription>
         </AlertDialogHeader>
