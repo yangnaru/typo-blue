1be1223abc3c9598f2a7e36f2db023fb29eb54f2
Jihyeok Seo <ji@hyeok.org>
2025-09-07 15:14:20 +0900

    @fedify/next

--- app/.well-known/webfinger/route.ts
+++
@@ -1,9 +0,0 @@
-import { fedifyRequestHandler } from "../../../lib/federation";
-
-export {
-  fedifyRequestHandler as DELETE,
-  fedifyRequestHandler as GET,
-  fedifyRequestHandler as PATCH,
-  fedifyRequestHandler as POST,
-  fedifyRequestHandler as PUT,
-};
