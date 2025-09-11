a772daaf0704899ca308668bca4a6c384bdc936d
Jihyeok Seo <ji@hyeok.org>
2025-09-11 17:49:12 +0900

    Fix logtape

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,6 +2,7 @@ import { configure, getConsoleSink } from "@logtape/logtape";
 
 export async function register() {
   await configure({
+    contextLocalStorage: new AsyncLocalStorage(),
     sinks: { console: getConsoleSink() },
     loggers: [
       {
