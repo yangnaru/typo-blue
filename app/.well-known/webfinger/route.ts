58102222936dc7c392760d83c26082e86e22aac3
Jihyeok Seo <ji@hyeok.org>
2025-10-23 19:22:30 +0900

    Revert "@fedify/next"
    This reverts commit 1be1223abc3c9598f2a7e36f2db023fb29eb54f2.

---
+++ app/.well-known/webfinger/route.ts
@@ -0,0 +1,9 @@
+import { fedifyRequestHandler } from "../../../lib/federation";
+
+export {
+  fedifyRequestHandler as DELETE,
+  fedifyRequestHandler as GET,
+  fedifyRequestHandler as PATCH,
+  fedifyRequestHandler as POST,
+  fedifyRequestHandler as PUT,
+};
